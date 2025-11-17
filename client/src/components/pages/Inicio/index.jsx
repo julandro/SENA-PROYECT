fb8193cedae8c3c2673f4ec3a4b9252e1319164a
Julian Camacho <julandro.mza@gmail.com>
2025-11-17 15:27:14 -0500

    Se mejora visualmente el home y se crea y ubica el logo del sistema

--- client/src/components/pages/Inicio/index.jsx
+++ client/src/components/pages/Inicio/index.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
-
+import React from 'react';
+import { Card, CardContent, Typography, Box } from '@mui/material';
+import { motion } from 'framer-motion';
+import { useAuth } from '../../../contexts/AuthContext';
+import LogoBlack2 from '../../../assets/img/LogoBlack2.png';
 const Inicio = () => {
+  const { user } = useAuth();
   return (
-    <div>Inicio</div>
-  )
-}
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="space-between"
+      alignItems="center"
+      height="100vh"
+    >
+      {/* LOGO ARRIBA */}
+      <motion.img
+        src={LogoBlack2}
+        alt="logo"
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 1 }}
+        style={{ width: 150 }}
+      />
 
-export default Inicio
+      {/* CENTRO */}
+      <motion.h1
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+      >
+        Bienvenido {user.username}
+      </motion.h1>
+
+      {/* ESPACIO FINAL (vacío) */}
+      <div style={{ height: 150 }} />
+    </Box>
+  );
+};
+
+export default Inicio;
